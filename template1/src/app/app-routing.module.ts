de335fbaf56c7d95e13150d6c67c4625b8d3959a
Manzura Yusupova <manzura@manzurasair133.attlocal.net>
2020-07-31 02:17:27 -0500

    template design works :)

--- template1/src/app/app-routing.module.ts
+++ template1/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './common/home/home.component';
+import { ContactUsComponent } from './common/contact-us/contact-us.component';
+import { AboutUsComponent } from './common/about-us/about-us.component';
+import { PageNotFoundComponent } from './design/page-not-found/page-not-found.component';
+import { ServicesComponent } from './common/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- template1/src/app/app.module.ts
+++ template1/src/app/app.module.ts
@@ -3,10 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './design/header/header.component';
+import { FooterComponent } from './design/footer/footer.component';
+import { NavComponent } from './design/nav/nav.component';
+
+import { CoverPageComponent } from './content/cover-page/cover-page.component';
+import { CardComponent } from './content/card/card.component';
+import { ContainerComponent } from './content/container/container.component';
+
+// Add to routing
+import { HomeComponent } from './common/home/home.component';
+import { ContactUsComponent } from './common/contact-us/contact-us.component';
+import { AboutUsComponent } from './common/about-us/about-us.component';
+import { PageNotFoundComponent } from './design/page-not-found/page-not-found.component';
+import { ServicesComponent } from './common/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    CoverPageComponent,
+    CardComponent,
+    ContainerComponent,
+    PageNotFoundComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
