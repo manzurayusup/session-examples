de8905c6830aa6c0f508730540218b4af9f61de1
Manzura Yusupova <manzura@manzurasair133.attlocal.net>
2020-08-04 12:50:28 -0500

    most styles working

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
-import { HomeComponent } from './common/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { NavComponent } from './common/nav/nav.component';
+
+//The following added to routing:
+import { HomeComponent } from './common/home/home.component';
 
 @NgModule({
   declarations: [
